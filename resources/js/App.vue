dfc4659e7693daacb7a85ab533ce13af6928b3bc
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-17 03:29:28 +0700

    📦 Properly implement Vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,19 @@
 <template>
-    Hello, {{ name }}
+    <main class='flex w-screen h-screen'>
+        <router-view></router-view>
+    </main>
 </template>
 
 <script lang='ts' setup>
 
-import { ref } from 'vue';
+import { Ref, ref } from 'vue';
+import User from './types/User';
+import Login from './Components/Login.vue';
 
-const name = ref("world!");
+type UserSession = Ref<User | undefined>;
+
+const user: UserSession = ref();
+
 
 </script>
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,22 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+import Login from '@/Components/Login.vue';
+import Register from '@/Components/Register.vue';
+
+const routes = [
+    { path: '/', component: Login },
+    { path: '/register', component: Register },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
 
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
 

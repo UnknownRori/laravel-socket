4eafa941e9fdac1a8a5e1b91f7f3f0329c7055ed
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-18 06:56:34 +0700

    📦 Added `SendChat` event

---
+++ app/Events/SendChat.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SendChat implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public User $user;
+    public Message $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Message $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PresenceChannel('chat');
+    }
+
+    public function broadcastAs()
+    {
+        return 'send-chat';
+    }
+}

0890ea332712d0618787b11fcddfecb2c80a78cf
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-03-18 06:57:55 +0700

    📦 Implement Auth using csrf

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(RegisterRequest $request): JsonResponse
+    {
+        $validated = $request->validated();
+
+        if (Auth::attempt($validated)) {
+            return response()->json([
+                'code' => 201,
+                'token' => auth()->user()->createToken('user')->plainTextToken,
+                'user' => new UserResource(auth()->user()),
+            ]);
+        }
+
+        return response()->json([
+            'status' => 400,
+            'message' => 'Account creation failed!',
+        ], 400);
+    }
+}
